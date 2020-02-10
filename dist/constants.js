a7da32e382a9c0afe002b50f65e42bbdb2403ae3
fastspeeed@gmail.com <fastspeeed@gmail.com>
2020-02-10 11:53:23 +0800

    remove dist

--- dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    APP_SECRET: 'appsecretmacoolka'
-};
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var apollo_server_micro_1 = require("apollo-server-micro");
-var schema_1 = require("./schema");
-var context_1 = require("./context");
-var apolloServer = new apollo_server_micro_1.ApolloServer({ schema: schema_1.schema, context: context_1.createContext });
-exports["default"] = apolloServer.createHandler({ path: '/api' });
